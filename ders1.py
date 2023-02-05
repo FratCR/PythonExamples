1e202d158c2a5ca9f60720404168b51b0825f6d7
Fırat Çakır <73741004+FratCR@users.noreply.github.com>
2023-02-05 21:33:38 +0300

    udemyLesson1
    my first Udemy Lesson notes

---
+++ ders1.py
@@ -0,0 +1,44 @@
+"""print(3+3)
+
+print("2"+"3")
+
+print(type(3.11)) #type hesaplamada .0 koyuldugu an her sey float olur
+
+print(3.5+6.8+9.2)
+"""
+
+#print(7000 + (7000 * 0.18))
+
+# urun1 = 120000
+# urun2 = 150000
+# kdv = 0.18
+
+# print(urun1 +(urun1 * kdv))
+# print(urun2 +(urun2 * kdv))
+
+#urun@ = 600 yanlıs bir kullanimdir, @ isareti degiskenlerde kullanilamaz
+
+#a, b, c = 10, 20, 30
+#print(a,b,c)
+
+#name = "Firat"
+#print(type(name))
+
+# isStudent = True
+# print(type(isStudent))
+
+# ad, soyad, yas, isStudent = "Firat", "Cakir", 18, True
+# print(ad,soyad,yas,isStudent)
+
+# age = 16
+# weight = 56.5
+# name = "Yagmur"
+# isStudent = True
+
+# print(type(age))
+# print(type(weight))
+# print(type(name))
+# print(type(isStudent))
+
+# print(float(age))
+# print(int(weight))
