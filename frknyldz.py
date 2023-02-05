70553e7d20f32a032d0864c6fa4220285e7494a2
Fırat Çakır <73741004+FratCR@users.noreply.github.com>
2023-02-05 21:49:48 +0300

    FurkanYıldız Notes
    Furkan Yıldız site notes

---
+++ frknyldz.py
@@ -0,0 +1,18 @@
+#matris oluşturulabilir
+"""for x in range (3):
+    for y in range (3):
+        print(x,y)"""
+        
+#fonksiyon kullanım metodları        
+"""def sayHello(name="Fratos"):
+    print("Hello",name)
+    
+sayHello("Firat")
+sayHello()"""
+
+"""def total(num1,num2):
+    return num1 + num2
+
+total (10,30)
+print (total)"""
+
