82ef1c1d682bd3f8df49bf825b116558fe4a7b67
Fırat Çakır <73741004+FratCR@users.noreply.github.com>
2023-03-26 02:46:28 +0300

    OOP Lesson
    OOP Turkish Lesson Notes with Python

---
+++ OOPpython1.py
@@ -0,0 +1,6 @@
+#Object-oriented programming temelde 4 ana koldan olusur:
+#1) Object(obje): bir obje, kisi, cisim, herhangi bir seyin icerisinde oldugu kapsamdir. # ornek: Firat
+#2) Class(sinif): objenin ait oldugu klasman, siniftir. Obje boyle tanimlanir. # ornek: insan
+#3) Properties (ozellikler): Objenin sahip oldugu ozelliklerdir, degistirilebilir. # ornek: gitari vardir, bacaklari vardir, arabasi vardir, bilgisayari vardir
+#4) Methods (metodlar): Objenin yapabildigi, gordugu islevdir. Neler yapip yapmayacagini burasi belirler. # ornek: gitar calabilir, kosabilir, araba surebilir, kod yazabilir
+
